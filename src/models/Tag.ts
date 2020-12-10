37deda2eea0b1ddfc5235d9ae9da31e9f6b78b13
Jacob Sood <hsoo6373@uni.sydney.edu.au>
2020-12-10 12:22:28 +1100

    Tag model created

---
+++ src/models/Tag.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ITag extends Document {
+    tag: string;
+}
+
+const TagSchema: Schema = new Schema({
+    tag: {
+        type: String,
+        required: true 
+    }
+});
+
+export default mongoose.model<ITag>("Tag", TagSchema);
